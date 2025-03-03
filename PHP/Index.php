a6f0ea2dfb2f3d8690109c3f7ef20c812bd61038
MAGGIE_CAHILL <g00423830@atu.ie>
2025-03-03 11:48:36 +0000

    Initial commit

---
+++ PHP/Index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+
+
+<link rel="stylesheet" href="../CSS/General_Styles.css">
+<link rel="stylesheet" href="../CSS/Home.css">
+
+
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+</style>
+
+
+</head>
+
+<body>
+
+<nav>
+    <div class="menu">
+
+        <div class="logo">
+            <img src="../Images/Final_Logo.png" alt="ATU Career Path Logo">
+        </div>
+
+        <ul class="navbar">
+            <li style = "font-size: 1.8em;"> <i class="fa-solid fa-envelope"></i> </li>
+            <li> Jobs </li>
+            <li> Appointments </li>
+            <li>
+                <button class="login"> Sign In </button>
+            </li>
+        </ul>
+    </div>
+    
+</nav>
+
+<div class="search">
+
+    <div class="box">
+        <h1>Pursue Your Aspirations and Find Your Future!</h1>
+        <p>Explore new and exciting opportunities for your career through ATU Career Path. The smooth transition between ATU 
+           certification to employment can be found here. Everything from summer internships to permanent job positions. 
+           Connect with employers today!
+        </p>
+
+        <form action="">
+            <div class="search-and-find">
+                <input type="text" class = "find">
+                <button type="submit"> 
+                    <i class="fa-solid fa-magnifying-glass"></i> 
+                </button>
+            </div>
+        </form>
+
+    </div>
+
+    <div class="backdrop"></div>
+    <div class="transparent"></div>
+  
+</div>
+
+<div class = "divider"></div>
+
+<div class="body-content">
+
+    <div class="subsection">
+        <div class="image-outline">
+            <img src="../Images/Office.jpg" alt="ATU Career Path Logo">
+        </div>
+
+        <div class="text-content">
+            <h1> Career Discovery </h1>
+
+            <div class = "underline">
+                <span></span>
+            </div>
+
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum. 
+            </p>
+        </div>
+    </div>
+
+    <div class="subsection">
+        <div class="text-content" style = "border-radius: 0px 50px 50px 0px; border-left: none; border-right: 10px double white;">
+            <h1 style = "text-align: left;"> Appointments </h1>
+            
+            <div class = "underline" style = "justify-content: start;">
+                <span></span>
+            </div>
+
+            <p style = "text-align: left;"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum. 
+            </p>
+        </div>
+
+        <div class="image-outline" style = "background-color: #008080;">
+            <img src="../Images/Office.jpg" alt="ATU Career Path Logo" style = "float: right; margin-right: 0px; margin-left: 20px; margin-bottom: 15px;">
+        </div>
+    </div>
+
+    <div class="subsection">
+        <div class="image-outline" style = "background-color: #B1D5E6;" >
+            <img src="../Images/Office.jpg" alt="ATU Career Path Logo">
+        </div>
+        
+        <div class="text-content">
+            <h1> Queries </h1>
+
+            <div class = "underline">
+                <span  style = "width: 25%;"></span>
+            </div>
+
+            <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate 
+                velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, 
+                sunt in culpa qui officia deserunt mollit anim id est laborum. 
+            </p>
+        </div>
+    </div>
+
+</div>
+
+<div class = "divider" style = "border-top: none; margin-top: 100px;"></div>
+
+
+<footer>
+    <div class="footer">
+        <div class="logo_background">
+            <img src="../Images/Final_Logo.png" alt="ATU Career Path Logo">
+            <p>© 2025 GTI Futures Ltd.</p>
+        </div>
+
+        <div class="footer-navigation">
+        
+            <ul>
+                <li>Legal</li>
+                <li>ATU Privacy Policy </li>
+                <li>Terms and Conditions </li>
+                <li>Accessibility</li>
+            </ul>
+
+            <ul>
+                <li>Useful Sites </li>
+                <li>GradIreland</li>
+                <li>LinkedIn</li>
+                <li>Indeed</li>
+                <li>Career Advice</li>
+            </ul>
+
+            <ul>
+                <li>Go To</li>
+                <li>Jobs </li>
+                <li>Appointments </li>
+                <li>Queries</li>
+                <li>Your Profile</li>
+            </ul>
+
+            <ul>
+                <li>Follow Us</li>
+                <li> <i class="fa-brands fa-linkedin"></i> LinkedIn</li>
+                <li> <i class="fa-brands fa-square-facebook"></i> Facebook</li>
+                <li> <i class="fa-brands fa-instagram"></i> Instagram</li>
+                <li> <i class="fa-brands fa-tiktok"></i> TikTok</li>
+            </ul>
+
+        </div>
+        
+    </div>
+</footer>
+
+
+</body>
+
+</html>
