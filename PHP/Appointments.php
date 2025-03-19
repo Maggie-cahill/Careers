9174a4e816a80dac402796f02bbb494b181e96a4
MAGGIE_CAHILL <g00423830@atu.ie>
2025-03-19 20:30:29 +0000

    appointments

--- PHP/Appointments.php
+++ PHP/Appointments.php
@@ -44,6 +44,7 @@
     </div>
 
     <div class="subtext">
+        <h1> Let's Start a Conversation!</h1>
         <p>
             "questions and concerns" is correct and usable in written English. You can use it to express that 
             something raises both questions and concerns--for example: The proposed changes to the health care 
@@ -53,13 +54,122 @@
         </p>
     </div>
 
-    <div class = "divider" style = "border-top: none; margin-top: 100px;"></div>
+    
+
+    <div class = "divider"></div>
 
     <div class="appointment-list">
+        <div class="hours-div">
+        <div class="hours">
+            <h1> Careers Office Hours</h1>
+            
+            
+
+            <table>
+                <tr>
+                    <th></th>
+                    <th> Monday </th>
+                    <th> Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                    <th>Sunday</th>
+                </tr>
+                <tr>
+                    <th>Opens</th>
+                    <td> 9:00 </td>
+                    <td> 9:00 </td>
+                    <td> 9:00 </td>
+                    <td> 9:00 </td>
+                    <td> 9:00 </td>
+                    <td></td>
+                    <td></td>
+                </tr>
+
+                <tr>
+                    <th></th>
+                    <td> to</td>
+                    <td> to</td>
+                    <td> to</td>
+                    <td> to</td>
+                    <td> to</td>
+                    <td>CLOSED</td>
+                    <td>CLOSED</td>
+                </tr>
+               
+                <tr>
+                    <th>Closes</th>
+                    <td> 17:00</td>
+                    <td> 17:00</td>
+                    <td> 17:00</td>
+                    <td> 17:00</td>
+                    <td> 16:00</td>
+                    <td></td>
+                    <td> </td>
+                </tr>
+                
+            </table>
+        </div>
+        </div>
         <div class="appointment-element">
             <img src="../Images/Office.jpg" alt="">
             <div class="text-content">
                 <h1> CV Check </h1>
+                <p> Random description for the appointment type and what it involves</p>
+                <button id = "request_booking">Request Booking</button>
+            </div>
+        </div>
+
+        <div class="appointment-element">
+            <img src="../Images/Office.jpg" alt="">
+            <div class="text-content">
+                <h1> Career Advice </h1>
+                <p> Random description for the appointment type and what it involves</p>
+                <button id = "request_booking">Request Booking</button>
+            </div>
+        </div>
+
+        <div class="appointment-element">
+            <img src="../Images/Office.jpg" alt="">
+            <div class="text-content">
+                <h1> Interview Prep </h1>
+                <p> Random description for the appointment type and what it involves</p>
+                <button id = "request_booking">Request Booking</button>
+            </div>
+        </div>
+
+        <div class="appointment-element">
+            <img src="../Images/Office.jpg" alt="">
+            <div class="text-content">
+                <h1> Vacancies</h1>
+                <p> Random description for the appointment type and what it involves</p>
+                <button id = "request_booking">Request Booking</button>
+            </div>
+        </div>
+
+        <div class="appointment-element">
+            <img src="../Images/Office.jpg" alt="">
+            <div class="text-content">
+                <h1> Applying For Jobs</h1>
+                <p> Random description for the appointment type and what it involves</p>
+                <button id = "request_booking">Request Booking</button>
+            </div>
+        </div>
+
+        <div class="appointment-element">
+            <img src="../Images/Office.jpg" alt="">
+            <div class="text-content">
+                <h1>  Looking For Part Time Work </h1>
+                <p> Random description for the appointment type and what it involves</p>
+                <button id = "request_booking">Request Booking</button>
+            </div>
+        </div>
+
+        <div class="appointment-element">
+            <img src="../Images/Office.jpg" alt="">
+            <div class="text-content">
+                <h1> Applying For Graduate Programs</h1>
                 <p> Random description for the appointment type and what it involves</p>
                 <button id = "request_booking">Request Booking</button>
             </div>
@@ -70,6 +180,48 @@
 </div>
 
 <div class = "divider" style = "border-top: none; margin-top: 100px;"></div>
+
+<div class="booking-form">
+    <div class="booking-form-content">
+
+        <form action="" id = "booking">
+            <div class="form-element">
+                <label for="date">Select a date:</label>
+                <input type="date" id="date" name = "date">
+            </div>
+
+            <div class="form-element">
+                <label for="time">Select a time:</label>
+                <input type="text" id="time" name = "time">
+            </div>
+
+            <div class="form-element">
+                <label for="describe">Describe your situation:</label>
+                <textarea name="describe" id="describe"></textarea>
+            </div>
+
+            <div class="form-element" id = "upload-stuff">
+                <label for="upload-files" style = "width: 100%;">Upload Files</label>
+                <div class="button-wrap">
+                    <label class = "upload-button" for = "upload-file"> Add Files &ensp;<i class="fa-solid fa-cloud-arrow-up"></i></label>
+                    <input type="file" id="upload-file" name="filename" multiple> 
+                </div>
+                <div id="file-list"></div>
+            </div>
+
+
+            <div class="form-element">
+                <span class = "separator"></span>
+            </div>
+
+            <div class="form-element" id = "submit-div">
+                <button type="submit" value = "Submit" id = "submit" >Send</button>
+            </div>
+
+
+        </form>
+    </div>
+</div>
 
 
 <footer>
